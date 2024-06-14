88f60520bddeba2f59d25ede399b6c995a0db686
Arylo <Arylo@users.noreply.github.com>
2024-06-14 13:01:19 +0000

    Deploying to monkey from @ Arylo/scripts@1d0ad847443970512e36bb55849a7e150b20c5ff 🚀

--- gitlab-settings-max-size.user.js
+++ gitlab-settings-max-size.user.js
@@ -3,8 +3,8 @@
 // @version 0.1.0
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @include /^https://git.[^/]+/.*/-/settings/ci_cd$/
-// @downloadURL https://raw.githubusercontent.com/Arylo/scripts//monkey/gitlab-settings-max-size.user.js
-// @updateURL https://raw.githubusercontent.com/Arylo/scripts//monkey/gitlab-settings-max-size.meta.js
+// @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/gitlab-settings-max-size.user.js
+// @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/gitlab-settings-max-size.meta.js
 // @run-at document-end
 // @grant none
 // ==/UserScript==
