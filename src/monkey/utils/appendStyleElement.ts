fa1e887b9998a678f65ee1e9a48f0d81d39d1b69
Doubley Yang <doubley_yang@epam.com>
2024-06-18 16:51:30 +0800

    chore: unlink the unused file

--- src/monkey/utils/appendStyleElement.ts
+++
@@ -1,7 +0,0 @@
-export const appendStyleElement = (cssContent: string, id?: string) => {
-  const elem = document.createElement('style')
-  id && (elem.id = id)
-  elem.innerHTML = cssContent
-
-  document.body.append(elem)
-}
