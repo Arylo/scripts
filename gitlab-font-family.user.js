af24a27bae1e38d6f1bbae23f2061fc51bf925a7
Arylo <Arylo@users.noreply.github.com>
2024-06-19 09:59:40 +0000

    Deploying to monkey from @ Arylo/scripts@d866d75cebbe9a69067588b2ac89f64cea124f7b 🚀

--- gitlab-font-family.user.js
+++ gitlab-font-family.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name Add Font Family for the Self-managed Gitlab
 // @name:zh 为自托管 Gitlab 添加字体
-// @version 2
+// @version 3
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @include /^https://git\b.[^/]+/.*/-/raw/.*/
 // @include /^https://git\b.[^/]+/.*/-/blob/.*/
 // @include /^https://git\b.[^/]+/.*/-/commit/.*/
 // @include /^https://git\b.[^/]+/.*/-/merge_requests/(\d+|new)\b/
 // @include /^https://git\b.[^/]+/.*/-/merge_requests/(\d+|new)/diffs\b/
+// @license MIT
+// @homepage https://github.com/Arylo/scripts#readme
+// @supportURL https://github.com/Arylo/scripts/issues
 // @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/gitlab-font-family.user.js
 // @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/gitlab-font-family.meta.js
 // @run-at document-end
--- nyaa-si-show-image.meta.js
+++ nyaa-si-show-image.meta.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name Display image right now in nyaa.si
 // @name:zh 在nyaa.si 上立即显示图片
-// @version 2
+// @version 3
 // @author Arylo Yeung <arylo.open@gmail.com>
+// @license MIT
 // @match https://sukebei.nyaa.si/view/*
 // @require https://code.jquery.com/jquery-3.6.0.min.js
+// @homepage https://github.com/Arylo/scripts#readme
+// @supportURL https://github.com/Arylo/scripts/issues
 // @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/nyaa-si-show-image.user.js
 // @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/nyaa-si-show-image.meta.js
 // @run-at document-end
