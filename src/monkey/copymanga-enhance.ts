8bba3953badae264df5fccb18ddde8431adb00e8
AryloYeung <arylo.open@gmail.com>
2024-06-22 16:08:30 +0800

    feat(copymanga): support session cache

--- src/monkey/copymanga-enhance.ts
+++ src/monkey/copymanga-enhance.ts
@@ -1,17 +1,28 @@
 import css from './copymanga-enhance.css'
 import html from './copymanga-enhance.html'
+import { comic } from './copymanga-enhance/common'
 import { render } from './copymanga-enhance/new'
 import { getPageInfo, refreshImage, windowScrollTo } from './copymanga-enhance/old'
 
+const renderNewPage = (info: any) => render({
+  info,
+  preFn: () => {
+    document.body.innerHTML = html
+    GM_addStyle(css)
+  },
+})
+
 setTimeout(() => {
+  let cacheContent = sessionStorage.getItem(`${comic}.info`)
+  let info
+  if (cacheContent) {
+    info = JSON.parse(cacheContent)
+    return renderNewPage(info)
+  }
   refreshImage(() => {
     windowScrollTo(0)
-    render({
-      info: getPageInfo(),
-      preFn: () => {
-        document.body.innerHTML = html
-        GM_addStyle(css)
-      },
-    })
+    info = getPageInfo()
+    sessionStorage.setItem(`${comic}.info`, JSON.stringify(info))
+    renderNewPage(info)
   })
 }, 25)
