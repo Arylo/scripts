ad900f60cfaba223f0937300956b68675949fbdc
Arylo <Arylo@users.noreply.github.com>
2024-08-22 05:26:19 +0000

    Deploying to monkey from @ Arylo/scripts@229a69b3595290cb1a5e587dbbcbe3645885a27f 🚀

--- copymanga-enhance.user.js
+++ copymanga-enhance.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance the copy manga site
-// @version 22
+// @version 23
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @license MIT
 // @match https://copymanga.com/comic/*/chapter/*
@@ -20,8 +20,8 @@
 // @require https://unpkg.com/vue@2.7.14/dist/vue.min.js
 // @homepage https://github.com/Arylo/scripts#readme
 // @supportURL https://github.com/Arylo/scripts/issues
-// @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/index.ts.user.js
-// @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/index.ts.meta.js
+// @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/copymanga-enhance.user.js
+// @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/copymanga-enhance.meta.js
 // @run-at document-end
 // @grant GM_addStyle
 // @grant GM_setValue
