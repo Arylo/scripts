371c111a41e9d6c6019fd68dffb99f99cc7238dd
Arylo <Arylo@users.noreply.github.com>
2024-09-01 08:34:13 +0000

    Deploying to monkey from @ Arylo/scripts@8f76ec1e86969f295111773f705c15e66f691139 🚀

--- copymanga-enhance.user.js
+++ copymanga-enhance.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance the copy manga site
-// @version 23
+// @version 24
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @license MIT
 // @match https://copymanga.com/comic/*/chapter/*
@@ -176,9 +176,11 @@
           case "ArrowDown".toLowerCase():
             that.onJumpPage("next_page" /* NEXT_PAGE */);
             break;
+          case "MetaLeft".toLowerCase():
           case "ControlLeft".toLowerCase():
             that.hasWhitePage = false;
             break;
+          case "MetaRight".toLowerCase():
           case "ControlRight".toLowerCase():
             that.hasWhitePage = true;
             break;
