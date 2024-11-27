67c2464c942cb6f6c3999d7d0945a1c71962e516
Arylo <Arylo@users.noreply.github.com>
2024-11-27 02:40:22 +0000

    Deploying to monkey from @ Arylo/scripts@bcca8b586d0b99b36504bf166be6ce7be63ae1df 🚀

--- gitlab-settings-max-size.meta.js
+++ gitlab-settings-max-size.meta.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name Max Size CI/CD Setting Page for the Self-managed Gitlab
 // @name:zh 自托管 Gitlab CI/CD 设置页面最大化尺寸
-// @version 7
+// @description (Deprecated)(Please use the gitlab-enhance script) Maximize the size of the CI/CD setting
+// @description:zh (已废弃)(请使用 gitlab-enhance 脚本) 最大化 CI/CD 设置页面的尺寸
+// @version 8
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @include /^https://git(lab)?.[^/]+/.*/-/settings/ci_cd$/
 // @license MIT
--- gitlab-settings-max-size.user.js
+++ gitlab-settings-max-size.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name Max Size CI/CD Setting Page for the Self-managed Gitlab
 // @name:zh 自托管 Gitlab CI/CD 设置页面最大化尺寸
-// @version 7
+// @description (Deprecated)(Please use the gitlab-enhance script) Maximize the size of the CI/CD setting
+// @description:zh (已废弃)(请使用 gitlab-enhance 脚本) 最大化 CI/CD 设置页面的尺寸
+// @version 8
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @include /^https://git(lab)?.[^/]+/.*/-/settings/ci_cd$/
 // @license MIT
@@ -42,9 +44,11 @@
     w.GM.addStyle = GM_addStyle;
   }
 
-  // src/monkey/gitlab-settings-max-size/style.css
-  var style_default = ".content-wrapper nav{max-width:100%}.content-wrapper .container-fluid{max-width:100%}.ci-variable-table table colgroup col:nth-child(3){width:100px}.ci-variable-table table colgroup col:nth-child(4){width:200px}.ci-variable-table table colgroup col:nth-child(5){width:50px}\n";
+  // src/monkey/gitlab-enhance/settings/ci_cd.css
+  var ci_cd_default = ".content-wrapper nav{max-width:100%}.content-wrapper .container-fluid{max-width:100%}.ci-variable-table table colgroup col:nth-child(3){width:100px}.ci-variable-table table colgroup col:nth-child(4){width:200px}.ci-variable-table table colgroup col:nth-child(5){width:50px}\n";
 
-  // src/monkey/gitlab-settings-max-size/index.ts
-  setTimeout(() => GM_addStyle(style_default), 25);
+  // src/monkey/gitlab-enhance/settings/ci_cd.ts
+  if (location.pathname.endsWith("/-/settings/ci_cd")) {
+    setTimeout(() => GM_addStyle(ci_cd_default), 25);
+  }
 })();
