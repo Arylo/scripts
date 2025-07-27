d563c8fd1dfdbd1ec05cea87bc2b10596bd9a145
Arylo <Arylo@users.noreply.github.com>
2025-07-27 04:47:23 +0000

    Deploying to monkey from @ Arylo/scripts@4d776e71d4a0dbee5dc823f46fc4b681c1ac54d7 🚀

--- copymanga-enhance.meta.js
+++ copymanga-enhance.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance the copy manga site
-// @version 29
+// @version 30
 // @author Arylo Yeung <arylo.open@gmail.com>
 // @license MIT
 // @match https://copymanga.com/comic/*/chapter/*
