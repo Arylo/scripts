9554991270fc6fe809b9231c7511d68cdced8eac
Arylo Yeung <arylo.open@gmail.com>
2025-12-24 00:58:31 +0800

    refactor(monkey): refactor nyaa-si-show-image image processing logic
    - extract oldProcess/newProcess/fallback functions
    - add compatibility for new image URL format
    - add multi-level fallback mechanism for stability

--- src/monkey/nyaa-si-show-image/index.ts
+++ src/monkey/nyaa-si-show-image/index.ts
@@ -1,39 +1,61 @@
+
+function fallback(elem, imgUrl) {
+  setTimeout(function () {
+    elem.attr("src", imgUrl + (imgUrl.indexOf("?") === -1 ? "?" : "&") + "refresh=" + Date.now());
+  }, Number(Math.random() * 2000));
+}
+
+function newProcess(elem, imgUrl, linkUrl) {
+  const linkList = linkUrl.split("/");
+  const imgList = imgUrl.split("/");
+  if (/^(\w+\.){2,}\w{2,}$/.test(imgList[3]) && imgList[4] === 'th') {
+    const [last] = imgList[imgList.length - 1].split('?');
+    imgList.splice(imgList.length - 1, 1, ...[last, linkList[linkList.length - 1]]);
+    imgList[4] = 'i';
+    imgList.splice(2, 1);
+    const newImgUrl = imgList.join('/');
+    elem.attr("src", newImgUrl);
+    elem.on("error", function () {
+      fallback(elem, newImgUrl);
+    });
+  }
+}
+
+function oldProcess(elem, imgUrl, linkUrl) {
+  if (!imgUrl.includes('//th')) return newProcess(elem, imgUrl, linkUrl);
+  let newImgUrl = imgUrl.replace('//th', '/i');
+  const list = linkUrl.split('/');
+  newImgUrl += '/';
+  newImgUrl += list[list.length - 1];
+  elem.attr('src', newImgUrl);
+  elem.on('error', function () {
+    newProcess(elem, imgUrl, linkUrl);
+  });
+}
+
 function handleUserJavsubs91() {
-  const markdownElement = $('div[markdown-text]')
+  const markdownElement = $('div[markdown-text]');
   if (markdownElement.length) {
-    const markdownMatches = markdownElement[0].innerText.match(/\[!\[.+\]\((.+)\)\]\((.+)\)/)
+    const markdownMatches = markdownElement[0].innerText.match(/\[!\[.+\]\((.+)\)\]\((.+)\)/);
     if (markdownMatches) {
-      const imageUrl = markdownMatches[1]
-      const targetUrl = markdownMatches[2]
-      markdownElement[0].innerHTML = `<a href="${targetUrl}"><img src="${imageUrl}"></a>`
+      const imageUrl = markdownMatches[1];
+      const targetUrl = markdownMatches[2];
+      markdownElement[0].innerHTML = `<a href="${targetUrl}"><img src="${imageUrl}"></a>`;
     }
   }
-  const imageElements = $('a > img')
+  const imageElements = $('a > img');
   if (imageElements.length) {
-    let imageSrc = imageElements.attr('src')
-    if (!imageSrc) return
-    const parentLinkUrl = imageElements.parent().attr('href')
-    if (!parentLinkUrl) return
-    imageSrc = imageSrc.replace('//th', '/i')
-    const urlSegments = parentLinkUrl.split('/')
-    imageSrc += '/'
-    imageSrc += urlSegments[urlSegments.length - 1]
-    imageElements.attr('src', imageSrc)
-    imageElements.on('error', function () {
-      setTimeout(function () {
-        const separator = imageSrc.indexOf('?') === -1 ? '?' : '&'
-        imageElements.attr('src', `${imageSrc}${separator}refresh=${Date.now()}`)
-      }, Number(Math.random() * 2000))
-    })
+    let imageSrc = imageElements.attr('src');
+    if (!imageSrc) return;
+    const parentLinkUrl = imageElements.parent().attr('href');
+    if (!parentLinkUrl) return;
+    oldProcess(imageElements, imageSrc, parentLinkUrl);
   }
 }
 
 setTimeout(() => {
-  const userLinks = $('.col-md-5 a[title="User"]')
-
+  const userLinks = $('.col-md-5 a[title="User"]');
   if (userLinks.length && userLinks.text() === 'javsubs91') {
-    switch (userLinks.text()) {
-      case 'javsubs91': handleUserJavsubs91()
-    }
+    handleUserJavsubs91();
   }
-}, 50)
+}, 50);
