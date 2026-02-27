46ed8968e3af4fe8a7704703fa89f570f7c82158
Arylo Yeung <arylo.open@gmail.com>
2026-02-27 14:56:57 +0800

    feat(monkey): detect Japanese titles for RTL and normalize greasyfork links

--- src/monkey/copymanga-enhance/scripts/table/index.ts
+++ src/monkey/copymanga-enhance/scripts/table/index.ts
@@ -1,6 +1,8 @@
 import { DirectionMode } from "../detail/newPage/constant"
 import { directionModeStorage, getDirectionModeKey } from "../../storages/directionMode"
 
+const hasJapanese = (text: string) => /[\u0800-\u4e00]/.test(text)
+
 export default () => {
   const directionModeKey = getDirectionModeKey()
   const directionMode = directionModeStorage.get(directionModeKey)
@@ -12,8 +14,22 @@ export default () => {
     return
   }
 
+  const bookName = $('h6').text()
+  if (hasJapanese(bookName)) {
+    console.log('Found Japanese book name')
+    directionModeStorage.set(directionModeKey, DirectionMode.RTL)
+    return
+  }
+
+  const bookAliasName = $('li:has(h6) + li > p').text()
+  if (hasJapanese(bookAliasName)) {
+    console.log('Found Japanese book alias name')
+    directionModeStorage.set(directionModeKey, DirectionMode.RTL)
+    return
+  }
+
   const authors = $('a[href^="/author"]').text()
-  if (/[\u0800-\u4e00]/.test(authors)) {
+  if (hasJapanese(authors)) {
     console.log('Found Japanese author')
     directionModeStorage.set(directionModeKey, DirectionMode.RTL)
     return
